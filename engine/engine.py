628e17e4b506d4dea7370f37efd89bd4fe59bee4
Multigriffes <jonas.t@laposte.net>
2026-02-03 12:21:43 +0100

    Refactor and cleaning

--- engine/engine.py
+++ engine/engine.py
@@ -3,14 +3,17 @@ from random import randint
 
 
 class VERTICES:
-    def __init__(self):
-        self.vertices=[]
-        self.normals=[]
+    def __init__(self,vertices=[],normals=[]):
+        self.vertices=vertices
+        self.normals=normals
         self.gl_list_id=None
+
     def set_vertices(self,vertices):
         self.vertices = vertices
+
     def set_normals(self,normals):
         self.normals = normals
+
     def compile(self):
         if glIsList(self.gl_list_id) == GL_FALSE:
             self.gl_list_id = glGenLists(1, GL_COMPILE)
@@ -22,6 +25,7 @@ class VERTICES:
             glEndList()
         else:
             print('Already compiled')
+
     def draw(self):
         if self.gl_list_id is not None:
             glCallList(self.gl_list_id)
@@ -29,20 +33,25 @@ class VERTICES:
             print('Not compiled')
 
 class FACES:
-    def __init__(self):
-        self.vertices=[]
-        self.normals=[]
-        self.triangles=[]
-        self.quads=[]
+    def __init__(self,vertices=[],normals=[],triangles=[],quads=[]):
+        self.vertices=vertices
+        self.normals=normals
+        self.triangles=triangles
+        self.quads=quads
         self.gl_list_id=None
+
     def set_vertices(self,vertices):
         self.vertices = vertices
+
     def set_normals(self,normals):
         self.normals = normals
+
     def set_triangles(self,triangles):
         self.triangles = triangles
+
     def set_quads(self,quads):
         self.quads = quads
+
     def compile(self):
         if self.gl_list_id is None:
             self.gl_list_id = glGenLists(1)
@@ -64,6 +73,7 @@ class FACES:
             glEndList()
         else:
             print('Already compiled')
+
     def draw(self):
         if self.gl_list_id is not None:
             glCallList(self.gl_list_id)
@@ -72,14 +82,17 @@ class FACES:
             self.compile()
 
 class LINES_LOOP:
-    def __init__(self):
-        self.vertices=[]
-        self.normals=[]
+    def __init__(self,vertices=[],normals=[]):
+        self.vertices=vertices
+        self.normals=normals
         self.gl_list_id=None
+
     def set_vertices(self,vertices):
         self.vertices = vertices
+
     def set_normals(self,normals):
         self.normals = normals
+
     def compile(self):
         if self.gl_list_id is None:
             self.gl_list_id = glGenLists(1)
@@ -91,6 +104,7 @@ class LINES_LOOP:
             glEndList()
         else:
             print('Already compiled')
+
     def draw(self):
         if self.gl_list_id is not None:
             glCallList(self.gl_list_id)
