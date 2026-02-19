4207bfb8ec59d8cdec30b2bec4e3a4005a5deb76
ChickenJumper4 <brogliapatronjulien@gmail.com>
2026-02-19 14:43:29 +0100

    ahhhhhhhhhhhhhhhhhhhhhhhhhh
    J'en peut tellement plus que j'ai croqué un bout de mon bureau pour gouter. LE PREMIER QUI ME DIT QUE C LE BORDEL DANS LE CODE JE LE TRUCIDE!!!!!!

--- cameras/configuration_camera.py
+++ cameras/configuration_camera.py
@@ -1,28 +1,27 @@
 import cv2
 import numpy as np
 from parameters import *
+from fonctions_images import *
+import csv
+from write_read_csv import *
 
 capture = cv2.VideoCapture(0)
+nb_photo = 0
+lst_points = []
 
 while capture.isOpened():
     ret, frame = capture.read()
     #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.flip(frame, 1)
-    if cv2.waitKey(1) == ord(take_photo):
+    if cv2.waitKey(1) == ord(take_photo) and nb_photo != nb_photo_max:
+        nb_photo += 1
         ret, corners = cv2.findChessboardCorners(frame, checkboard_info[0],
                 cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+        lst_points.append(corners)
         for i in corners:
             cv2.circle(frame, (int(i[0][0]), int(i[0][1])), 5, (0, 255, 0), 2)
         cv2.imshow('a', frame)
 
-        objp = np.zeros((6*8, 3), np.float32)
-        objp[:,:2] = np.mgrid[0:6,0:8].T.reshape(-1,2)
-        objectPoints = [objp]
- 
-        imagePoints = [corners]
- 
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objectPoints, imagePoints,
-                                    (frame[0].shape[1], frame[0].shape[0]), None, None)
 
     if cv2.waitKey(1) == ord(quitter):
         break
@@ -30,3 +29,7 @@ while capture.isOpened():
         cv2.imshow("test", frame)
 
 capture.release()
+
+K = compute_intrinsics(object_points_list, lst_points)
+
+write(K)
