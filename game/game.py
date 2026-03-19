d983f76be2d30f81f6dc83503af56b5e885ed847
ChickenJumper4 <brogliapatronjulien@gmail.com>
2026-03-19 15:05:08 +0100

    ajout des class objet

--- game/game.py
+++ game/game.py
@@ -1,10 +1,9 @@
-# Importer OBJ_FILE
+from engine.dot_obj_parser import OBJ_FILE
 
-class Block:
+class Objet:
     def __init__(self, color:tuple, pos:list, obj_path=None):
         self.color = color
         self.pos = pos
-        self.size = 100
         
         if obj_path != None:
             self.obj = OBJ_FILE(obj_path)
@@ -15,27 +14,37 @@ class Block:
     def get_pos(self):
         return self.pos
     
-    def get_size(self):
-        return self.size
-    
     def get_obj(self):
         return self.obj
-
-    def detect_collision(self, object_pos_lst:list)->bool:
-        x, y,  z = self.pos
-        size = self.size
-        
-        for pos in object_pos_lst:
-            x_in = x-size/2 <= pos[0] <= x+size/2
-            y_in = y-size/2 <= pos[1] <= y+size/2
-            z_in = z-size/2 <= pos[2] <= z+size/2
-            
-            if x_in and y_in and z_in:
-                return True
-            
-        return False
     
     def move(self, translation:tuple):
         self.pos[0] += translation(0)
         self.pos[1] += translation(1)
         self.pos[2] += translation(2)
+
+class Block(Objet):
+    def __init__(self):
+        self.size = 100
+        super().__init__()
+
+    def get_size(self):
+        return self.size
+
+    def detect_collision(self, object_pos_lst: list) -> bool:
+        x, y, z = self.pos
+        size = self.size
+
+        for pos in object_pos_lst:
+            x_in = x - size / 2 <= pos[0] <= x + size / 2
+            y_in = y - size / 2 <= pos[1] <= y + size / 2
+            z_in = z - size / 2 <= pos[2] <= z + size / 2
+
+            if x_in and y_in and z_in:
+                return True
+
+        return False
+
+
+class Controller(Objet):
+    def __init__(self, color:tuple, pos:list, obj_path:str):
+        super().__init__(color, pos, obj_path)
