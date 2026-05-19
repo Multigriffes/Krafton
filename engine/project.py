50a54e7cb340b59f68332ee6611f06e4b0b7faef
Multigriffes <jonas.t@laposte.net>
2026-05-19 11:02:27 +0200

    suppression inutile

--- engine/project.py
+++ engine/project.py
@@ -2,8 +2,6 @@
 debug_axes = False
 debug_rotation_axes = False
 objects = {
-    #'Backpack' : {'type': 'Faces', 'path': 'Krafton/3d_models/scene/environnement.obj', 'coordinates': [30,10,0], 'color': None, 'to_be_drew': True},
-    #'Environnement' : {'type': 'Faces', 'path': '3d_models/scene/environnement.obj', 'coordinates': [0,0,0], 'color': None, 'to_be_drew': True, "collide_box": None},
     'Block_1' : {'type': 'Faces', 'path': '3d_models/scene/Block.obj', 'coordinates': [0, 0, -30], 'color': None, "collide_box": [[0,0,0],[1,1,1]]},
     'Block_2' : {'type': 'Faces', 'path': '3d_models/scene/Block.obj', 'coordinates': [10, 0, -30], 'color': None, "collide_box": [[0,0,0],[1,1,1]]},
     'Block_3' : {'type': 'Faces', 'path': '3d_models/scene/Block.obj', 'coordinates': [20, 0, -30], 'color': None, "collide_box": [[0,0,0],[1,1,1]]},
